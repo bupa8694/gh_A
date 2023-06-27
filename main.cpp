40b749951cc2e3889f96b4cca0a94a298e95d425
Macx Buddhi Chaturanga <pkbchaturanga@gmail.com>
2023-06-27 06:04:46 +0530

    change1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>  // NOLINT
+
+/**
+ * The main function of a C++ program.
+ *
+ * @return int The exit status code of the program.
+ *
+ * @throws None
+ */
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+return 0;
+}
